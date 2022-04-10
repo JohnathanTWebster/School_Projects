025de4f06f82763e319320f6f108ec9032f7eb33
Johnathan Webster <81039525+JohnathanTWebster@users.noreply.github.com>
2022-04-09 21:26:11 -0400

    Updated file on onedrive

--- Coding Projects/Assignment 1-1/src/App.java
+++
@@ -1,6 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to Java \nWelcome to Computer Science \nProgramming is Fun");
-    }
-}
-
